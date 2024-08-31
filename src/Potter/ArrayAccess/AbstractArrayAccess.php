9a4758c5685926734104b7b18bb4184a48266604
Jay Potter <j@ypotter.ca>
2024-08-30 20:33:48 -0400

    Create AbstractArrayAccess.php

---
+++ src/Potter/ArrayAccess/AbstractArrayAccess.php
@@ -0,0 +1,13 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Potter\ArrayAccess;
+
+abstract class AbstractArrayAccess implements ArrayAccessInterface
+{
+    abstract public function offsetExists(mixed $offset): bool;
+    abstract public function offsetGet(mixed $offset): mixed;
+    abstract public function offsetSet(mixed $offset, mixed $value): void;
+    abstract public function offsetUnset(mixed $offset): void;
+}
