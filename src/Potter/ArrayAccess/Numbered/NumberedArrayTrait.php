82402ff6d7faa7a734c4f704bee4089ec015e9ea
Jay Potter <j@ypotter.ca>
2024-09-01 16:58:55 -0400

    Update NumberedArrayTrait.php

--- src/Potter/ArrayAccess/Numbered/NumberedArrayTrait.php
+++ src/Potter/ArrayAccess/Numbered/NumberedArrayTrait.php
@@ -30,11 +30,9 @@ trait NumberedArrayTrait
     
     final public function valid(): bool
     {
-        return offsetExists($this->index);
+        return $this->offsetExists($this->index);
     }
     
     abstract public function offsetExists(mixed $offset): bool;
     abstract public function offsetGet(mixed $offset): mixed;
-    abstract public function offsetSet(mixed $offset, mixed $value): void;
-    abstract public function offsetUnset(mixed $offset): void;
 }
